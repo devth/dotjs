05a4ed769b1299507d2e3d2f05e8a653ceefc49e
Trevor Hartman <trevorhartman@gmail.com>
2011-10-11 10:57:33 -0700

    Add initial dotjs for pitchfork and rally

---
+++ rally1.rallydev.com.js
@@ -0,0 +1,16 @@
+$(function() {
+  setTimeout(function(){
+    $(".x4-container.x4-container-default.x4-border-layout-ct").css('backgroundImage', 'url(http://cl.ly/330y1x2N272J0U2M420q/suffer.jpg)')
+      // .css('backgroundPositionX', '80%')
+      .css('background-repeat', 'repeat-x')
+      // .hover(function() {
+        // $(this).animate({ height: 217, });
+      // }, function() {
+        // $(this).animate({ height: 88, });
+      // });
+
+    // $("#x4-ext-gen1019").hide();
+
+    $("body").css("background", "url(http://images.memegenerator.net/instances/400x/10111529.jpg) repeat");
+  }, 2000);
+});
