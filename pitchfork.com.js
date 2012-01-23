83e0208a3072c7a079dcb71fb5d6cfca40e83523
Trevor Hartman <trevorhartman@gmail.com>
2012-01-23 10:46:58 -0800

    Fix syntax for Pitchfork spotify links

--- pitchfork.com.js
+++ pitchfork.com.js
@@ -1,15 +1,22 @@
 $(function() {
-  var $ri = $(".review-info");
+  // TODO - lookup album with XHR first http://developer.spotify.com/en/metadata-api/overview/
+  //
+  var $ri = $(".review-meta .info");
   if ($ri.length === 1) {
-    var search = $("h1, h2", $ri).text().replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/g, " ").trim();
-    if (search !== "" && search !== []) {
+    // replace(/(\r\n|\n|\r)/gm," ").replace(/\s+/g, " ")
+    var artist = $ri.find("h1:first").text().trim(),
+        album = $ri.find("h2:first").text().trim(),
+        search = "album:" + encodeURIComponent(album);
+    if (search !== "") {
       $("body").prepend($("<div />")
         .html($("<div />")
           .html($("<a />")
             .attr("href", "spotify:search:'" + search + "'")
             .attr("style", "color: white;")
-            .html("<b>" + search + "</b>" + " on Spotify"))
+            .html("<b>" + album + "</b>" + " on Spotify"))
           .attr("style", "position: fixed; z-index: 2005999999; top: 0; right: 0; background: black; padding: 10px; color: white !important;")));
+    } else {
+      console.warn("Could not parse album", album, artist);
     }
   }
 });
