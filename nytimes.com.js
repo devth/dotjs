4944a90efc076523e2dfc8f880e268951da78a7d
Trevor Hartman <trevorhartman@gmail.com>
2012-01-26 11:00:45 -0800

    Add nytimes.com.js to hide dictionary icon when selecting text

---
+++ nytimes.com.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+  // Hide annoying dictionary icon that appears when selecting text
+  $('head')
+    .append($("<style type='text/css' />")
+    .html("#nytd_selection_button_wordReference { display: none !important; }"));
+});
